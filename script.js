de65b89708f825e568ccd03c7b6be126b64a7c1a
Daniel Cuadra <daniel.cuadra85@gmail.com>
2020-09-28 20:39:01 -0700

    shape framework of weather

--- script.js
+++ script.js
@@ -1,11 +1,33 @@
-var APIKey = "38ce9d2bdb41501b21e8be2d27c623c6&units=imperial&units=kelvin";
+$(document).ready(function () {
+
+var APIKey = "38ce9d2bdb41501b21e8be2d27c623c6&units=imperial&units=imperial";
 
 var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=Bujumbura,Burundi&appid=" + APIKey;
 
-$.ajax({
-  url: queryURL,
-  method: "GET"
-}).then(function(response) {
-  console.log(response);
+
+    $('search-button').on('click', function() {
+        console.log(this)
+        var citySearch = $('#search-value').val();
+        $('#search-value').val('')
+    
+    })
+
+
+    function weatherSearch(citySearch) {
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+
+    }).then(function(response) {
+        console.log(response);
+        
+    })
+        var newCard = $('<div>').addClass('card')
+
+        var wind = $('<p>').addClass('card-text').text ('Wind' + DataCue.wind.speed + 'mph')
+    }
+
+
 
 })
